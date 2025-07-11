4300d213a2a1e353cb7a0da45d6945994f6afd00
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-12 01:04:23 +0530

    this class implement AuthenticationSuccessHandler

--- Backend/src/main/java/com/DeepThreat/Authentication/CustomOAuth2SuccessHandler.java
+++ Backend/src/main/java/com/DeepThreat/Authentication/CustomOAuth2SuccessHandler.java
@@ -1,7 +1,8 @@
 package com.DeepThreat.Authentication;
 
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CustomOAuth2SuccessHandler {
+public class CustomOAuth2SuccessHandler implements AuthenticationSuccessHandler {
 }
