3750c3e8a35cc02e177d0622642188fcdf43967e
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-12 01:23:43 +0530

    add a class that helps for OAuth

---
+++ Backend/src/main/java/com/DeepThreat/Authentication/CustomOAuth2UserService.java
@@ -0,0 +1,39 @@
+package com.DeepThreat.Authentication;
+
+
+import com.DeepThreat.Entity.UserEntity;
+import com.DeepThreat.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = new DefaultOAuth2UserService().loadUser(userRequest);
+
+        String email = oAuth2User.getAttribute("email");
+        String name = oAuth2User.getAttribute("name");
+        Optional<UserEntity> ue = userRepository.findById(email);
+        if(ue.isEmpty()){
+            UserEntity userEntity = new UserEntity();
+            userEntity.setName(name);
+            userEntity.setEmailId(email);
+            userEntity.setPassword("GOOGLE_AUTH");
+
+            userRepository.save(userEntity);
+        }
+        return  oAuth2User;
+    }
+}
