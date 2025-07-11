f21c22744e4fa214f114718c5f11b65098cc1f09
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-12 01:26:18 +0530

    add password encoder to the custom OAUTH

--- Backend/src/main/java/com/DeepThreat/Authentication/CustomOAuth2UserService.java
+++ Backend/src/main/java/com/DeepThreat/Authentication/CustomOAuth2UserService.java
@@ -4,6 +4,7 @@ package com.DeepThreat.Authentication;
 import com.DeepThreat.Entity.UserEntity;
 import com.DeepThreat.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
@@ -19,6 +20,9 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = new DefaultOAuth2UserService().loadUser(userRequest);
@@ -30,7 +34,7 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
             UserEntity userEntity = new UserEntity();
             userEntity.setName(name);
             userEntity.setEmailId(email);
-            userEntity.setPassword("GOOGLE_AUTH");
+            userEntity.setPassword(passwordEncoder.encode("GOOGLE_AUTH"));
 
             userRepository.save(userEntity);
         }
