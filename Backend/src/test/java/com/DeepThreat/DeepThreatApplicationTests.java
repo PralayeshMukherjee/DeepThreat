a600b200d9fa82f71a3e00d9b4fab3c3443dde83
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-11 23:49:11 +0530

    add the backend init folder

---
+++ Backend/src/test/java/com/DeepThreat/DeepThreatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DeepThreat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepThreatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
