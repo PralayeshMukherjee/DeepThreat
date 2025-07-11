aaf27c1784ee74e5a47373d5b0d11b6cc4b98777
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-12 00:36:06 +0530

    make the two instance that auto inject the value from application.properties

--- Backend/src/main/java/com/DeepThreat/Authentication/JwtUtil.java
+++ Backend/src/main/java/com/DeepThreat/Authentication/JwtUtil.java
@@ -1,7 +1,14 @@
 package com.DeepThreat.Authentication;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${jwt.expiration}")
+    private long expirationMs;
 }
