2f4bc4787c212f245cabeb578b6e6392abfc2e65
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-12 01:15:45 +0530

    complete the security filter chain

--- Backend/src/main/java/com/DeepThreat/Config/SecurityConfig.java
+++ Backend/src/main/java/com/DeepThreat/Config/SecurityConfig.java
@@ -1,9 +1,32 @@
 package com.DeepThreat.Config;
 
+import com.DeepThreat.Authentication.CustomOAuth2SuccessHandler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    private final CustomOAuth2SuccessHandler successHandler;
+
+    public SecurityConfig(CustomOAuth2SuccessHandler successHandler) {
+        this.successHandler = successHandler;
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+                .cors(Customizer.withDefaults())
+                .csrf(csrf->csrf.disable())
+                .authorizeHttpRequests(auth->auth
+                        .requestMatchers("/","/login/**","/oauth2/**","/user").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2->oauth2
+                        .successHandler(successHandler));
+        return http.build();
+    }
 }
