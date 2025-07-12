08595641b2fece5ba2ad9652a992b3c0fc4b5724
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-12 13:23:00 +0530

    add the annotation of rest controller

--- Backend/src/main/java/com/DeepThreat/Controller/RedirectController.java
+++ Backend/src/main/java/com/DeepThreat/Controller/RedirectController.java
@@ -5,12 +5,16 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
+@RestController
 public class RedirectController {
     @Autowired
     private JwtUtil jwtUtil;
+    @GetMapping("/redirect")
     public void redirectAfterLogin(HttpServletResponse response, Authentication authentication) throws IOException{
         OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
         String jwtToken = jwtUtil.generateTokan(oAuth2User);
