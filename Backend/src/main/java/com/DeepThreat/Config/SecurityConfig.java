d4e8651b42155d96008916f6e94df801bc8c6a96
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-13 19:21:03 +0530

    add a new request matchers

--- Backend/src/main/java/com/DeepThreat/Config/SecurityConfig.java
+++ Backend/src/main/java/com/DeepThreat/Config/SecurityConfig.java
@@ -22,6 +22,7 @@ public class SecurityConfig {
                 .csrf(csrf->csrf.disable())
                 .authorizeHttpRequests(auth->auth
                         .requestMatchers("/","/login/**","/oauth2/**","/user/**").permitAll()
+                        .requestMatchers("/api/token-validation").authenticated()
                         .anyRequest().authenticated()
                 )
                 .oauth2Login(oauth2->oauth2
