3016b6ea82a76497b2cbc95320508f85fd1486c1
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-13 19:51:25 +0530

    add the instance of Jwt Utils

--- Backend/src/main/java/com/DeepThreat/Controller/JwtValidation.java
+++ Backend/src/main/java/com/DeepThreat/Controller/JwtValidation.java
@@ -1,10 +1,9 @@
 package com.DeepThreat.Controller;
 
+import com.DeepThreat.Authentication.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 import java.util.Map;
@@ -12,11 +11,10 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api")
 public class JwtValidation {
+    @Autowired
+    private JwtUtil jwtUtil;
     @GetMapping("/token-validation")
-    public ResponseEntity<Map<String,String>> tokenValidation (Principal principal){
-        return ResponseEntity.ok(Map.of(
-                "isTokenValid","true",
-                "email",principal.getName()
-        ));
+    public Map<String,String> tokenValidation (@RequestParam String token){
+
     }
 }
