4b5ee3fcc3677a07ef49634c179c7bcf9bd5177b
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-13 19:25:31 +0530

    update map

---
+++ Backend/src/main/java/com/DeepThreat/Controller/JwtValidation.java
@@ -0,0 +1,21 @@
+package com.DeepThreat.Controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class JwtValidation {
+    @PostMapping("/token-validation")
+    public ResponseEntity<Map<String,String>> tokenValidation (Principal principal){
+        return ResponseEntity.ok(Map.of(
+                "isTokenValid","true",
+                "email",principal.getName()
+        ));
+    }
+}
