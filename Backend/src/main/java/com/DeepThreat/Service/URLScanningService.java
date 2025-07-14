dcffa0f06ea0a5a1dd23d0145087ede622261470
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-15 01:02:01 +0530

    add a method that return 3 if some words matches else 0

--- Backend/src/main/java/com/DeepThreat/Service/URLScanningService.java
+++ Backend/src/main/java/com/DeepThreat/Service/URLScanningService.java
@@ -1,8 +1,17 @@
 package com.DeepThreat.Service;
 
 import org.springframework.stereotype.Service;
-
+//login, account, verify, update, secure, bank, free, gift, c
 @Service
 public class URLScanningService {
+    public int suspiciousKeywordsChecks(String url){
+        if(url.contains("login")||url.contains("account")||url.contains("verify")||url.contains("update")||url.contains("secure")||url.contains("bank")||url.contains("free")||url.contains("gift")||url.contains("prize")){
+            return 3;
+        }else{
+            return 0;
+        }
+    }
+    public int finalPointStable(String url){
 
+    }
 }
