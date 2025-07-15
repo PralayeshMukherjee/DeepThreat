631c5ae615733fc57f7879c49ef1c47e9431901b
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-15 23:50:00 +0530

    autowired the instance

--- Backend/src/main/java/com/DeepThreat/Controller/URLCheckingController.java
+++ Backend/src/main/java/com/DeepThreat/Controller/URLCheckingController.java
@@ -2,6 +2,7 @@ package com.DeepThreat.Controller;
 
 import com.DeepThreat.DTO.URLDTO;
 import com.DeepThreat.Service.URLScanningService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/urlChecker")
 public class URLCheckingController {
+    @Autowired
     private URLScanningService urlScanningService;
     public Map<String,String> urlChecker(@RequestBody URLDTO urldto){
 
