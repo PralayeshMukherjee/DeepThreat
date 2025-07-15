d222f4d0fbe544fcd4e535adabe07ce6d64527a3
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-15 23:57:58 +0530

    add the getmapping

--- Backend/src/main/java/com/DeepThreat/Controller/URLCheckingController.java
+++ Backend/src/main/java/com/DeepThreat/Controller/URLCheckingController.java
@@ -3,6 +3,7 @@ package com.DeepThreat.Controller;
 import com.DeepThreat.DTO.URLDTO;
 import com.DeepThreat.Service.URLScanningService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import java.util.Map;
 public class URLCheckingController {
     @Autowired
     private URLScanningService urlScanningService;
+    @GetMapping("/check")
     public Map<String,String> urlChecker(@RequestBody URLDTO urldto){
         String url = urldto.getUrl();
         String result = urlScanningService.domainChecker(url);
