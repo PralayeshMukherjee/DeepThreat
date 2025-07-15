af7b42125bed847026509212b2043819a416b401
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-15 23:49:46 +0530

    add the instance of the URL Scanning service

--- Backend/src/main/java/com/DeepThreat/Controller/URLCheckingController.java
+++ Backend/src/main/java/com/DeepThreat/Controller/URLCheckingController.java
@@ -1,6 +1,7 @@
 package com.DeepThreat.Controller;
 
 import com.DeepThreat.DTO.URLDTO;
+import com.DeepThreat.Service.URLScanningService;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/urlChecker")
 public class URLCheckingController {
+    private URLScanningService urlScanningService;
     public Map<String,String> urlChecker(@RequestBody URLDTO urldto){
 
     }
