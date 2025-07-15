a16d75a489b7e9976175c10c4b719033eaa5bbd1
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-15 23:40:42 +0530

    write a method for the url checker

--- Backend/src/main/java/com/DeepThreat/Controller/URLCheckingController.java
+++ Backend/src/main/java/com/DeepThreat/Controller/URLCheckingController.java
@@ -3,7 +3,12 @@ package com.DeepThreat.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/urlChecker")
 public class URLCheckingController {
+    public Map<String,String> urlChecker(){
+
+    }
 }
