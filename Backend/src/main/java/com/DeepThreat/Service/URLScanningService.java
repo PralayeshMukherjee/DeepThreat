d315e2d86c8b8baf15adc4399f5ddf77aa2fc195
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-15 23:16:00 +0530

    add a method that check the mismatch of domain

--- Backend/src/main/java/com/DeepThreat/Service/URLScanningService.java
+++ Backend/src/main/java/com/DeepThreat/Service/URLScanningService.java
@@ -59,6 +59,9 @@ public class URLScanningService {
         }
         return 0;
     }
+    public int mismatchedTopLevelDomain(String url){
+
+    }
 //    public int finalPointStable(String url){
 //        int pointCheck = 0;
 //        pointCheck += suspiciousKeywordsChecks(url);
