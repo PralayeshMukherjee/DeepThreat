2103420b37acb30adf72d6902035503adc7fd22c
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-19 01:41:48 +0530

    make the repository as url entity

--- Backend/src/main/java/com/DeepThreat/Repository/URLHistoryRepository.java
+++ Backend/src/main/java/com/DeepThreat/Repository/URLHistoryRepository.java
@@ -1,7 +1,7 @@
 package com.DeepThreat.Repository;
 
-import com.DeepThreat.Entity.URLHistory;
+import com.DeepThreat.Entity.URLHistoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface URLHistoryRepository extends JpaRepository<URLHistory,Long> {
+public interface URLHistoryRepository extends JpaRepository<URLHistoryEntity,Long> {
 }
