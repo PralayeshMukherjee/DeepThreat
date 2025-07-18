28856771f15bd923c8af028116eb2643b6432fe0
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-18 23:41:46 +0530

    create the private data members

--- Backend/src/main/java/com/DeepThreat/Entity/URLHistory.java
+++ Backend/src/main/java/com/DeepThreat/Entity/URLHistory.java
@@ -3,7 +3,15 @@ package com.DeepThreat.Entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
+import java.sql.Date;
+
 @Entity
 @Table(name = "urlhistor")
 public class URLHistory {
+    private Long serialNo;
+    private String url;
+    private Date date;
+    private int safe;
+    private int suspicious;
+    private int malicious;
 }
