58ba88439edaebe7144d9f7187eccc736f0ca7a1
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-19 23:06:11 +0530

    create the private instance of threat detection service layer

--- Backend/src/main/java/com/DeepThreat/Controller/URLCheckingController.java
+++ Backend/src/main/java/com/DeepThreat/Controller/URLCheckingController.java
@@ -1,6 +1,7 @@
 package com.DeepThreat.Controller;
 
 import com.DeepThreat.DTO.URLDTO;
+import com.DeepThreat.Service.ThreatDetection;
 import com.DeepThreat.Service.URLScanningService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +13,7 @@ import java.util.Map;
 public class URLCheckingController {
     @Autowired
     private URLScanningService urlScanningService;
+    private ThreatDetection threatDetection;
     @PostMapping("/check")
     public Map<String,String> urlChecker(@RequestBody URLDTO urldto){
         String url = urldto.getUrl();
