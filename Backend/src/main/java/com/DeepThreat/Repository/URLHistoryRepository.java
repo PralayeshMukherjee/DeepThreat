46395ef171fbcc3b62f5b52e42f10655b5a0e3b0
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-19 21:15:35 +0530

    write the custom query to get data by url

--- Backend/src/main/java/com/DeepThreat/Repository/URLHistoryRepository.java
+++ Backend/src/main/java/com/DeepThreat/Repository/URLHistoryRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface URLHistoryRepository extends JpaRepository<URLHistoryEntity,Long> {
-    @Query()
+    @Query("SELECT * FROM urlhistory WHERE url = :url, nativeQuery=true")
     URLHistoryEntity findByURL(@Param("url")String url);
 }
