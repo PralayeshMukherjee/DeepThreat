a56445777318a42be45845999a839845c4237a15
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-19 21:10:57 +0530

    add the query annotation for custom annotation

--- Backend/src/main/java/com/DeepThreat/Repository/URLHistoryRepository.java
+++ Backend/src/main/java/com/DeepThreat/Repository/URLHistoryRepository.java
@@ -2,8 +2,10 @@ package com.DeepThreat.Repository;
 
 import com.DeepThreat.Entity.URLHistoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface URLHistoryRepository extends JpaRepository<URLHistoryEntity,Long> {
+    @Query()
     URLHistoryEntity findByURL(@Param("url")String url);
 }
