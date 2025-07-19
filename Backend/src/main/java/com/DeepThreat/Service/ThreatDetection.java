bff2271d3f62a9574cb4614af1b7385dd3687ba5
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-19 20:41:44 +0530

    add the instance of the url history repository by the help of it I can interact with database

--- Backend/src/main/java/com/DeepThreat/Service/ThreatDetection.java
+++ Backend/src/main/java/com/DeepThreat/Service/ThreatDetection.java
@@ -1,7 +1,9 @@
 package com.DeepThreat.Service;
 
+import com.DeepThreat.Repository.URLHistoryRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ThreatDetection {
+    private URLHistoryRepository urlHistoryRepository;
 }
