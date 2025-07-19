6a563fa093d72226b0966af6e72e9626b04a753d
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-19 23:27:40 +0530

    add the repository annotation to make this class as a repository class

--- Backend/src/main/java/com/DeepThreat/Repository/URLHistoryRepository.java
+++ Backend/src/main/java/com/DeepThreat/Repository/URLHistoryRepository.java
@@ -3,10 +3,13 @@ package com.DeepThreat.Repository;
 import com.DeepThreat.Entity.URLHistoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface URLHistoryRepository extends JpaRepository<URLHistoryEntity,Long> {
     @Query("SELECT * FROM urlhistory WHERE url = :url, nativeQuery=true")
     List<URLHistoryEntity> findByURL(@Param("url")String url);
