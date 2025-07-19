64c2c51c8acab90e6bc36a0d759e61d5265dd4e7
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-19 21:10:06 +0530

    add the parameter that take the value of url

--- Backend/src/main/java/com/DeepThreat/Repository/URLHistoryRepository.java
+++ Backend/src/main/java/com/DeepThreat/Repository/URLHistoryRepository.java
@@ -2,7 +2,8 @@ package com.DeepThreat.Repository;
 
 import com.DeepThreat.Entity.URLHistoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface URLHistoryRepository extends JpaRepository<URLHistoryEntity,Long> {
-    URLHistoryEntity findByURL();
+    URLHistoryEntity findByURL(@Param("url")String url);
 }
