d78f90422ff62aa502bd8118290a02b7e4c163c9
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-19 21:07:57 +0530

    create a method that get the element by url

--- Backend/src/main/java/com/DeepThreat/Repository/URLHistoryRepository.java
+++ Backend/src/main/java/com/DeepThreat/Repository/URLHistoryRepository.java
@@ -4,4 +4,5 @@ import com.DeepThreat.Entity.URLHistoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface URLHistoryRepository extends JpaRepository<URLHistoryEntity,Long> {
+    URLHistoryEntity findByURL();
 }
