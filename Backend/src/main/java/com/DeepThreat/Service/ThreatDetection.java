33f9f7777934b8b5fe97fe93fc6fda2bd67bce2c
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-19 21:18:37 +0530

    call the method that fetch data by the help of url by interact with jpa repository

--- Backend/src/main/java/com/DeepThreat/Service/ThreatDetection.java
+++ Backend/src/main/java/com/DeepThreat/Service/ThreatDetection.java
@@ -1,16 +1,18 @@
 package com.DeepThreat.Service;
 
+import com.DeepThreat.Entity.URLHistoryEntity;
 import com.DeepThreat.Repository.URLHistoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
+import java.util.Optional;
 
 @Service
 public class ThreatDetection {
     @Autowired
     private URLHistoryRepository urlHistoryRepository;
     public Map<String,String> isThreatDetect(String url){
-
+        Optional<URLHistoryEntity> uhe = urlHistoryRepository.findByURL(url);
     }
 }
