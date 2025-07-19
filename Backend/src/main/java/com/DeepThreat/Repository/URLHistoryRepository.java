310933cf4e21f6ce4cd64a3683cca1eed5368c65
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-19 22:13:00 +0530

    change the holding item from entity to List of entity because it hold a bunch of data of same entity

--- Backend/src/main/java/com/DeepThreat/Repository/URLHistoryRepository.java
+++ Backend/src/main/java/com/DeepThreat/Repository/URLHistoryRepository.java
@@ -5,7 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface URLHistoryRepository extends JpaRepository<URLHistoryEntity,Long> {
     @Query("SELECT * FROM urlhistory WHERE url = :url, nativeQuery=true")
-    URLHistoryEntity findByURL(@Param("url")String url);
+    List<URLHistoryEntity> findByURL(@Param("url")String url);
 }
