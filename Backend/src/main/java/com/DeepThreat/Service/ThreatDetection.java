cd3bc838e275c435fe65d0123ecd17f78ad5dcf0
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-19 20:57:22 +0530

    create a method that return Map of (string,string) and detect threat

--- Backend/src/main/java/com/DeepThreat/Service/ThreatDetection.java
+++ Backend/src/main/java/com/DeepThreat/Service/ThreatDetection.java
@@ -4,8 +4,13 @@ import com.DeepThreat.Repository.URLHistoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 @Service
 public class ThreatDetection {
     @Autowired
     private URLHistoryRepository urlHistoryRepository;
+    public Map<String,String> isThreatDetect(String url){
+
+    }
 }
