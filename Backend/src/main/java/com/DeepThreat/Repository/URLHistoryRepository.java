c058f2a42629512c8ba00565e874b49a04d16f53
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-21 23:55:12 +0530

    change the query

--- Backend/src/main/java/com/DeepThreat/Repository/URLHistoryRepository.java
+++ Backend/src/main/java/com/DeepThreat/Repository/URLHistoryRepository.java
@@ -3,14 +3,12 @@ package com.DeepThreat.Repository;
 import com.DeepThreat.Entity.URLHistoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface URLHistoryRepository extends JpaRepository<URLHistoryEntity,Long> {
-    @Query(value = "SELECT * FROM urlhistory WHERE url = :url", nativeQuery=true)
-    List<URLHistoryEntity> findByURL(@Param("url")String url);
+    @Query(value = "SELECT * FROM urlhistory WHERE url = ?1",nativeQuery = true)
+    List<URLHistoryEntity> findByUrl(String url);
 }
