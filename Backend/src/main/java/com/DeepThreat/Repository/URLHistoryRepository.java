cdf77ca872b27bd28ad74e51167d46627303d1fa
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-22 00:53:13 +0530

    remove the unwanted query

--- Backend/src/main/java/com/DeepThreat/Repository/URLHistoryRepository.java
+++ Backend/src/main/java/com/DeepThreat/Repository/URLHistoryRepository.java
@@ -2,13 +2,11 @@ package com.DeepThreat.Repository;
 
 import com.DeepThreat.Entity.URLHistoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface URLHistoryRepository extends JpaRepository<URLHistoryEntity,Long> {
-    @Query(value = "SELECT * FROM urlhistory WHERE url = ?1",nativeQuery = true)
     List<URLHistoryEntity> findByUrl(String url);
 }
