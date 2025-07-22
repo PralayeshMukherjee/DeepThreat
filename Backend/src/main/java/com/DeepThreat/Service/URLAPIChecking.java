a0bfb19a9ba1f38837cd18f2b48d99c83ae7e5ce
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-22 23:05:15 +0530

    add the value annotation that fetch the value from application.properties and auto inject in the private data members

--- Backend/src/main/java/com/DeepThreat/Service/URLAPIChecking.java
+++ Backend/src/main/java/com/DeepThreat/Service/URLAPIChecking.java
@@ -1,8 +1,10 @@
 package com.DeepThreat.Service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class URLAPIChecking {
+    @Value("${google.safe.browsing.api.key}")
     private String googleSafeBrowsingApiKey;
 }
