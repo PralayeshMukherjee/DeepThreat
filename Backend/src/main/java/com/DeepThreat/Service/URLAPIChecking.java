5ebca6ce4f00b13dd3b6d47afd14a81ce448d9eb
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-22 23:44:49 +0530

    put the data in map which act like body of the request

--- Backend/src/main/java/com/DeepThreat/Service/URLAPIChecking.java
+++ Backend/src/main/java/com/DeepThreat/Service/URLAPIChecking.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -15,5 +16,16 @@ public class URLAPIChecking {
     private final RestTemplate restTemplate = new RestTemplate();
     public boolean isGoogleSafeBrowsing(String url){
         Map<String,Object> map = new HashMap<>();
+        map.put("client", Map.of(
+                "clientId", "deepthreat-app",
+                "clientVersion", "1.0"
+        ));
+
+        map.put("threatInfo", Map.of(
+                "threatTypes", List.of("MALWARE", "SOCIAL_ENGINEERING", "UNWANTED_SOFTWARE"),
+                "platformTypes", List.of("ANY_PLATFORM"),
+                "threatEntryTypes", List.of("URL"),
+                "threatEntries", List.of(Map.of("url", url))
+        ));
     }
 }
