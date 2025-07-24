ab09259cca281389b8c3c4deaf41fb8ef5f63037
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-25 00:23:56 +0530

    autowired the instance of repository for dependency injection

--- Backend/src/main/java/com/DeepThreat/Service/HistoryService.java
+++ Backend/src/main/java/com/DeepThreat/Service/HistoryService.java
@@ -1,10 +1,11 @@
 package com.DeepThreat.Service;
 
 import com.DeepThreat.Repository.URLHistoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HistoryService {
-
+    @Autowired
     private URLHistoryRepository urlHistoryRepository;
 }
