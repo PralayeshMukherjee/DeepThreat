86cceb89a59bdf4b12bf4537feeb4ba3e90273db
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-25 01:11:04 +0530

    add the repository annotation to make the interface as repository layer

--- Backend/src/main/java/com/DeepThreat/Repository/UserURLHistoryRepository.java
+++ Backend/src/main/java/com/DeepThreat/Repository/UserURLHistoryRepository.java
@@ -2,6 +2,8 @@ package com.DeepThreat.Repository;
 
 import com.DeepThreat.Entity.UserURLHistoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserURLHistoryRepository extends JpaRepository<UserURLHistoryEntity,Long> {
 }
