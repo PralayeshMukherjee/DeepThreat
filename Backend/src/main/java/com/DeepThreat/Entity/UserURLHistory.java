7e20044d7d73483db945d34fd96f500dde66a878
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-25 00:50:53 +0530

    add the id annotation to make the serial number as primary key and it is auto generated and unique

--- Backend/src/main/java/com/DeepThreat/Entity/UserURLHistory.java
+++ Backend/src/main/java/com/DeepThreat/Entity/UserURLHistory.java
@@ -1,11 +1,17 @@
 package com.DeepThreat.Entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 import java.sql.Date;
 
 @Entity
 public class UserURLHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long serialNo;
     private String email;
     private String url;
     private Date date;
