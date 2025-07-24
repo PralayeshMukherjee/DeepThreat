2596980ddbb98fd42fd0c052762a4bf289a846a9
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-25 00:49:00 +0530

    create all private data members for the entity layer

--- Backend/src/main/java/com/DeepThreat/Entity/UserURLHistory.java
+++ Backend/src/main/java/com/DeepThreat/Entity/UserURLHistory.java
@@ -2,6 +2,14 @@ package com.DeepThreat.Entity;
 
 import jakarta.persistence.Entity;
 
+import java.sql.Date;
+
 @Entity
 public class UserURLHistory {
+    private String email;
+    private String url;
+    private Date date;
+    private int malicious;
+    private int Suspicious;
+    private int safe;
 }
