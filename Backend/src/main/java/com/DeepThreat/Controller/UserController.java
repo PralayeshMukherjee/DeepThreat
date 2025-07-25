de4ea6ed9d1159303e398f3f6106fa29dead44aa
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-26 00:46:41 +0530

    add the instance of History service

--- Backend/src/main/java/com/DeepThreat/Controller/UserController.java
+++ Backend/src/main/java/com/DeepThreat/Controller/UserController.java
@@ -1,10 +1,11 @@
 package com.DeepThreat.Controller;
 
+import com.DeepThreat.Service.HistoryService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/userDetails")
 public class UserController {
-
+    private HistoryService historyService;
 }
