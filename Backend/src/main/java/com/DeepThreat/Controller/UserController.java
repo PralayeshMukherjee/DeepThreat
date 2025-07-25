218694bd9aa43b794165dca386d6277d200f9888
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-26 00:47:17 +0530

    add the annotation of autowired

--- Backend/src/main/java/com/DeepThreat/Controller/UserController.java
+++ Backend/src/main/java/com/DeepThreat/Controller/UserController.java
@@ -1,11 +1,13 @@
 package com.DeepThreat.Controller;
 
 import com.DeepThreat.Service.HistoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/userDetails")
 public class UserController {
+    @Autowired
     private HistoryService historyService;
 }
