8a2235aa77dd41119ecdb187baf926e765abcd42
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-26 00:51:11 +0530

    add the post mapping annotations

--- Backend/src/main/java/com/DeepThreat/Controller/UserController.java
+++ Backend/src/main/java/com/DeepThreat/Controller/UserController.java
@@ -3,6 +3,7 @@ package com.DeepThreat.Controller;
 import com.DeepThreat.Entity.UserURLHistoryEntity;
 import com.DeepThreat.Service.HistoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import java.util.List;
 public class UserController {
     @Autowired
     private HistoryService historyService;
+    @PostMapping("/urlHistory")
     public List<UserURLHistoryEntity> urlHistory(@RequestParam String email){
         return historyService.urlSearchedHistory(email);
     }
