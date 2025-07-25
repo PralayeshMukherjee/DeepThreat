1ebd250a7f7dca3ec1817fcefda9179aaf33613f
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-26 00:48:17 +0530

    add a public method that take the email and return the list of user url history entity

--- Backend/src/main/java/com/DeepThreat/Controller/UserController.java
+++ Backend/src/main/java/com/DeepThreat/Controller/UserController.java
@@ -1,13 +1,20 @@
 package com.DeepThreat.Controller;
 
+import com.DeepThreat.Entity.UserURLHistoryEntity;
 import com.DeepThreat.Service.HistoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/userDetails")
 public class UserController {
     @Autowired
     private HistoryService historyService;
+    public List<UserURLHistoryEntity> urlHistory(@RequestParam String email){
+
+    }
 }
