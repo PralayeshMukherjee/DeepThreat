c3c238162902879da306a421fb516cb9c87f8b76
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-26 00:44:17 +0530

    add the annotation of request mapping

--- Backend/src/main/java/com/DeepThreat/Controller/UserController.java
+++ Backend/src/main/java/com/DeepThreat/Controller/UserController.java
@@ -1,7 +1,10 @@
 package com.DeepThreat.Controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/userDetails")
 public class UserController {
+
 }
