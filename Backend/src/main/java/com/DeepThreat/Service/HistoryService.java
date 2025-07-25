000ba7194775a1c0b1439d165dbf4c7f0e4891b9
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-26 00:12:06 +0530

    get all the data that user searched and store it in a list

--- Backend/src/main/java/com/DeepThreat/Service/HistoryService.java
+++ Backend/src/main/java/com/DeepThreat/Service/HistoryService.java
@@ -1,15 +1,18 @@
 package com.DeepThreat.Service;
 
+import com.DeepThreat.Entity.UserURLHistoryEntity;
 import com.DeepThreat.Repository.URLHistoryRepository;
 import com.DeepThreat.Repository.UserURLHistoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class HistoryService {
     @Autowired
     private UserURLHistoryRepository userURLHistoryRepository;
     public void urlSearchedHistory(String email){
-
+        List<UserURLHistoryEntity> list = userURLHistoryRepository.findAll();
     }
 }
