d8bd68572f594af2f1e18dc3a7854d85a454ff49
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-29 01:40:00 +0530

    add the private instance of user repository

--- Backend/src/main/java/com/DeepThreat/Service/UserDetailsService.java
+++ Backend/src/main/java/com/DeepThreat/Service/UserDetailsService.java
@@ -1,7 +1,9 @@
 package com.DeepThreat.Service;
 
+import com.DeepThreat.Repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserDetailsService {
+    private UserRepository userRepository;
 }
