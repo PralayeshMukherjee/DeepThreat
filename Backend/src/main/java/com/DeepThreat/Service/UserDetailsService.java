7a194b6204cc4b31b3975783b3dea946ca99e865
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-29 01:40:37 +0530

    add the autowired annotation to make the dependency injection

--- Backend/src/main/java/com/DeepThreat/Service/UserDetailsService.java
+++ Backend/src/main/java/com/DeepThreat/Service/UserDetailsService.java
@@ -1,9 +1,11 @@
 package com.DeepThreat.Service;
 
 import com.DeepThreat.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserDetailsService {
+    @Autowired
     private UserRepository userRepository;
 }
