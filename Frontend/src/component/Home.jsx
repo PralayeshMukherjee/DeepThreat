9d125ec35504af9c03e850fe69027950850bdcf1
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-29 00:52:03 +0530

    import the useState hook

--- Frontend/src/component/Home.jsx
+++ Frontend/src/component/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { FaShieldAlt, FaLock, FaUpload } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
