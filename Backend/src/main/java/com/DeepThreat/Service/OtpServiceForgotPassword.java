e157eb7c7da60d8d95081226261687dc75b760c4
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-29 23:54:23 +0530

    store the otp in a map data structure

--- Backend/src/main/java/com/DeepThreat/Service/OtpServiceForgotPassword.java
+++ Backend/src/main/java/com/DeepThreat/Service/OtpServiceForgotPassword.java
@@ -4,9 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Service
 public class OtpServiceForgotPassword {
     @Autowired
     private JavaMailSender javaMailSender;
-    private static final int otpLenght = 6
+    private static final int otpLenght = 6;
+    private Map<String,String > otpMapping = new HashMap<>();
 }
