dcd4a5eebd27411444ec7f25f9bd248c31699410
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-29 23:55:02 +0530

    add a method that generate otp

--- Backend/src/main/java/com/DeepThreat/Service/OtpServiceForgotPassword.java
+++ Backend/src/main/java/com/DeepThreat/Service/OtpServiceForgotPassword.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class OtpServiceForgotPassword {
@@ -13,4 +16,15 @@ public class OtpServiceForgotPassword {
     private JavaMailSender javaMailSender;
     private static final int otpLenght = 6;
     private Map<String,String > otpMapping = new HashMap<>();
+    public boolean generateOTP(String emailId){
+        String generatedOTP = "";
+        for(int i=1;i<=otpLenght;i++){
+            generatedOTP += String.valueOf((int)(Math.random()*9)+1);
+        }
+        otpMapping.put(emailId,generatedOTP);
+        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
+        scheduledExecutorService.schedule(()->otpMapping.remove(emailId),3, TimeUnit.MINUTES);
+//       the ScheduledExecutorService is a java utility that used to schedule the task. It basically schedule any task execute after a define time stamp you can decide the time stamp here I decide minutes. It is better than Treade.sleep() method because it doesnot block the main tread. Executors.newScheduledThreadPool(1) means it create a thread pool with 1 background thread to handle the schedule task.
+        return true;
+    }
 }
