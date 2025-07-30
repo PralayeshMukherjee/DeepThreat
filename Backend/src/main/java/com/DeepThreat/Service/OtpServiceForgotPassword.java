898394be15c8957074fdc021f08be67112549680
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-07-31 00:42:12 +0530

    change the return type from String to Map of (String,String)

--- Backend/src/main/java/com/DeepThreat/Service/OtpServiceForgotPassword.java
+++ Backend/src/main/java/com/DeepThreat/Service/OtpServiceForgotPassword.java
@@ -109,7 +109,7 @@ public class OtpServiceForgotPassword {
             return false;
         }
     }
-    public String verifyOTP(String emailId, String otp,String newPassword){
+    public Map<String,String> verifyOTP(String emailId, String otp,String newPassword){
         if(otpMapping.containsKey(emailId)){
             if(otpMapping.get(emailId).equals(otp)){
                 otpMapping.remove(emailId);
