95f45bd2c227ab8fb6dab3cd0e0fee8547c44515
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-08-02 00:59:16 +0530

    add the encoded url base on Base64

--- Backend/src/main/java/com/DeepThreat/Service/VirusTotalService.java
+++ Backend/src/main/java/com/DeepThreat/Service/VirusTotalService.java
@@ -3,6 +3,8 @@ package com.DeepThreat.Service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.util.Base64;
+
 @Service
 public class VirusTotalService {
     @Value("${virustotal.api.key}")
@@ -10,6 +12,6 @@ public class VirusTotalService {
     @Value("${virustotal.api.url}")
     private String apiUrl;
     public int virusTotalCheck(String url){
-
+        String encodedUrl = Base64.getUrlEncoder().encodeToString(url.getBytes());
     }
 }
