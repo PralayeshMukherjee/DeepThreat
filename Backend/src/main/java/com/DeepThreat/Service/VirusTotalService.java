1c37c664b8400b7730f4f6c232fcb2e7e223c198
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-08-02 01:07:11 +0530

    add the try-catch block to handle the error

--- Backend/src/main/java/com/DeepThreat/Service/VirusTotalService.java
+++ Backend/src/main/java/com/DeepThreat/Service/VirusTotalService.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.net.URI;
+import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.Base64;
 
 @Service
@@ -14,9 +16,15 @@ public class VirusTotalService {
     @Value("${virustotal.api.url}")
     private String apiUrl;
     public int virusTotalCheck(String url){
-        String encodedUrl = Base64.getUrlEncoder().encodeToString(url.getBytes());
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(apiUrl+"/"+encodedUrl))
-                .header("x-apikey",apikey).build();
+        try{
+            String encodedUrl = Base64.getUrlEncoder().encodeToString(url.getBytes());
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(apiUrl+"/"+encodedUrl))
+                    .header("x-apikey",apikey).build();
+            HttpResponse<String> response = HttpClient.newHttpClient()
+                    .send(request,HttpResponse.BodyHandlers.ofString());
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 }
