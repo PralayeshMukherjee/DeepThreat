232bc0356984cae67ee485bd01b090e157fa0c04
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-08-02 01:01:53 +0530

    build the Http Request

--- Backend/src/main/java/com/DeepThreat/Service/VirusTotalService.java
+++ Backend/src/main/java/com/DeepThreat/Service/VirusTotalService.java
@@ -3,6 +3,8 @@ package com.DeepThreat.Service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.net.URI;
+import java.net.http.HttpRequest;
 import java.util.Base64;
 
 @Service
@@ -13,5 +15,8 @@ public class VirusTotalService {
     private String apiUrl;
     public int virusTotalCheck(String url){
         String encodedUrl = Base64.getUrlEncoder().encodeToString(url.getBytes());
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(apiUrl+"/"+encodedUrl))
+                .header("x-apikey",apikey).build();
     }
 }
