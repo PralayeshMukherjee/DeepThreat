f4dd5445c28f91864716b7f70cd6e33c6fe35d35
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-08-09 21:41:01 +0530

    add the Param annotation for parameters

--- Backend/src/main/java/com/DeepThreat/Repository/UserURLHistoryRepository.java
+++ Backend/src/main/java/com/DeepThreat/Repository/UserURLHistoryRepository.java
@@ -2,6 +2,7 @@ package com.DeepThreat.Repository;
 
 import com.DeepThreat.Entity.UserURLHistoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,5 +10,5 @@ import java.util.Optional;
 
 @Repository
 public interface UserURLHistoryRepository extends JpaRepository<UserURLHistoryEntity,Long> {
-    List<UserURLHistoryEntity> findLastThreeURL();
+    List<UserURLHistoryEntity> findLastThreeURL(@Param("email")String email);
 }
