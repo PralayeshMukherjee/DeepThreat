77ab8daf83f8e5700e14b02eff4d42a0cb138a63
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-08-09 22:32:18 +0530

    call the method of find last 3 data from database

--- Backend/src/main/java/com/DeepThreat/Service/LastThreeHistoryService.java
+++ Backend/src/main/java/com/DeepThreat/Service/LastThreeHistoryService.java
@@ -1,6 +1,7 @@
 package com.DeepThreat.Service;
 
 import com.DeepThreat.DTO.UserPastData;
+import com.DeepThreat.Entity.UserURLHistoryEntity;
 import com.DeepThreat.Repository.UserURLHistoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,6 @@ public class LastThreeHistoryService {
     @Autowired
     private UserURLHistoryRepository userURLHistoryRepository;
     public List<UserPastData> getLastThreeHistory(String email){
-
+        List<UserURLHistoryEntity> getDataFromDataBase = userURLHistoryRepository.findLastThreeURL(email);
     }
 }
