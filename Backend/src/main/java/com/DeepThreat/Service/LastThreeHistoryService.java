d7963681a6c08fe03c82a98bb2ea1fb6cce21049
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-08-09 22:15:32 +0530

    autowired the private instance

--- Backend/src/main/java/com/DeepThreat/Service/LastThreeHistoryService.java
+++ Backend/src/main/java/com/DeepThreat/Service/LastThreeHistoryService.java
@@ -1,9 +1,11 @@
 package com.DeepThreat.Service;
 
 import com.DeepThreat.Repository.UserURLHistoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LastThreeHistoryService {
+    @Autowired
     private UserURLHistoryRepository userURLHistoryRepository;
 }
