8e722516bdcce71fc1760d26d92e12de128ed6ea
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-08-09 23:27:09 +0530

    create a function that send the last three datas

--- Backend/src/main/java/com/DeepThreat/Controller/LastThreeData.java
+++ Backend/src/main/java/com/DeepThreat/Controller/LastThreeData.java
@@ -1,11 +1,20 @@
 package com.DeepThreat.Controller;
 
+import com.DeepThreat.DTO.UserPastData;
 import com.DeepThreat.Service.LastThreeHistoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 
 @RestController
 public class LastThreeData {
     @Autowired
     private LastThreeHistoryService lastThreeHistoryService;
+    public ResponseEntity<List<UserPastData>> lastThreeDatas(@RequestParam String email){
+
+    }
 }
