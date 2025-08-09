3b1c5a4e9be630e9be34029cddcbae9f41cc0bd4
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-08-09 23:19:44 +0530

    create the private instance of LastThreeDataService class

--- Backend/src/main/java/com/DeepThreat/Controller/LastThreeData.java
+++ Backend/src/main/java/com/DeepThreat/Controller/LastThreeData.java
@@ -1,7 +1,9 @@
 package com.DeepThreat.Controller;
 
+import com.DeepThreat.Service.LastThreeHistoryService;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LastThreeData {
+    private LastThreeHistoryService lastThreeHistoryService;
 }
