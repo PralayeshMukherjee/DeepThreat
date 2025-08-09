475b00127a10b66d60f48f2c97a90d4a953c239b
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-08-09 23:34:20 +0530

    add the request mapping

--- Backend/src/main/java/com/DeepThreat/Controller/LastThreeData.java
+++ Backend/src/main/java/com/DeepThreat/Controller/LastThreeData.java
@@ -4,6 +4,7 @@ import com.DeepThreat.DTO.UserPastData;
 import com.DeepThreat.Service.LastThreeHistoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ import java.util.List;
 
 
 @RestController
+@RequestMapping("/lastData")
 public class LastThreeData {
     @Autowired
     private LastThreeHistoryService lastThreeHistoryService;
