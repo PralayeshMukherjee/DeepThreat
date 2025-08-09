9b4f8c376b72f238ec6f81ab662f89faffcece50
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-08-09 23:20:16 +0530

    autowired it to make the dependency injection

--- Backend/src/main/java/com/DeepThreat/Controller/LastThreeData.java
+++ Backend/src/main/java/com/DeepThreat/Controller/LastThreeData.java
@@ -1,9 +1,11 @@
 package com.DeepThreat.Controller;
 
 import com.DeepThreat.Service.LastThreeHistoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LastThreeData {
+    @Autowired
     private LastThreeHistoryService lastThreeHistoryService;
 }
