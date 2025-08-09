057fd48c55bb7788fa74c916699e7296cb3a0208
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-08-09 22:22:53 +0530

    create a new public function which return the list of User past data

--- Backend/src/main/java/com/DeepThreat/Service/LastThreeHistoryService.java
+++ Backend/src/main/java/com/DeepThreat/Service/LastThreeHistoryService.java
@@ -1,11 +1,17 @@
 package com.DeepThreat.Service;
 
+import com.DeepThreat.DTO.UserPastData;
 import com.DeepThreat.Repository.UserURLHistoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class LastThreeHistoryService {
     @Autowired
     private UserURLHistoryRepository userURLHistoryRepository;
+    public List<UserPastData> getLastThreeHistory(String email){
+
+    }
 }
