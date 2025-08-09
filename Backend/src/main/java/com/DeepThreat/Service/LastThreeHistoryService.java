79d9ef142242bdfd47eefa9add5f025612122f0b
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-08-09 22:14:52 +0530

    create the private instance of User url history repository

--- Backend/src/main/java/com/DeepThreat/Service/LastThreeHistoryService.java
+++ Backend/src/main/java/com/DeepThreat/Service/LastThreeHistoryService.java
@@ -1,7 +1,9 @@
 package com.DeepThreat.Service;
 
+import com.DeepThreat.Repository.UserURLHistoryRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LastThreeHistoryService {
+    private UserURLHistoryRepository userURLHistoryRepository;
 }
