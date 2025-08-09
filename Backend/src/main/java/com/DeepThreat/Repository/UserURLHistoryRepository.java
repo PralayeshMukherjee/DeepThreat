f611c732d5fdc88ffb0e99872325c6c025716d15
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-08-09 21:39:51 +0530

    return list of UserURLHistoryEntity

--- Backend/src/main/java/com/DeepThreat/Repository/UserURLHistoryRepository.java
+++ Backend/src/main/java/com/DeepThreat/Repository/UserURLHistoryRepository.java
@@ -4,7 +4,10 @@ import com.DeepThreat.Entity.UserURLHistoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface UserURLHistoryRepository extends JpaRepository<UserURLHistoryEntity,Long> {
-    UserURLHistoryEntity findLastThreeURL();
+    List<UserURLHistoryEntity> findLastThreeURL();
 }
