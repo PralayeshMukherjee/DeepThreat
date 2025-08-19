1294ca6ae5988e4764de48037487b27619bba48d
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-08-19 18:01:26 +0530

    create the private instance of File Scanner Service to use it locally

--- Backend/src/main/java/com/DeepThreat/Controller/FileCheckerController.java
+++ Backend/src/main/java/com/DeepThreat/Controller/FileCheckerController.java
@@ -1,6 +1,7 @@
 package com.DeepThreat.Controller;
 
 import com.DeepThreat.DTO.FileDTO;
+import com.DeepThreat.Service.FileScannerService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +12,9 @@ import java.util.Map;
 @RestController
 @RequestMapping("/fileScanning")
 public class FileCheckerController {
+    private FileScannerService fileScannerService;
     public ResponseEntity<Map<String,String>> FileScan(FileDTO fileDTO){
         File file = fileDTO.getFile();
+
     }
 }
