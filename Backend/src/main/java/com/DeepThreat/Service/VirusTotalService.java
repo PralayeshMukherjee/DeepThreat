89c23cc86bcf9dbd9970f2cc70e025c9336dd5a2
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-08-19 14:35:45 +0530

    use retrieve method to get the data

--- Backend/src/main/java/com/DeepThreat/Service/VirusTotalService.java
+++ Backend/src/main/java/com/DeepThreat/Service/VirusTotalService.java
@@ -88,5 +88,6 @@ public class VirusTotalService {
         return webClient.get()
                 .uri(newCompleteURLForAnalysis)
                 .header("x-apikey",apikey)
+                .retrieve()
     }
 }
