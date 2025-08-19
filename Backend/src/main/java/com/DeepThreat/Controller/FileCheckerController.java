d0e1134bb6959d1e10af13c20be3633ac3a7c8e3
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-08-19 18:02:03 +0530

    use autowired annotation to make automatically dependency injection

--- Backend/src/main/java/com/DeepThreat/Controller/FileCheckerController.java
+++ Backend/src/main/java/com/DeepThreat/Controller/FileCheckerController.java
@@ -2,6 +2,7 @@ package com.DeepThreat.Controller;
 
 import com.DeepThreat.DTO.FileDTO;
 import com.DeepThreat.Service.FileScannerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/fileScanning")
 public class FileCheckerController {
+    @Autowired
     private FileScannerService fileScannerService;
     public ResponseEntity<Map<String,String>> FileScan(FileDTO fileDTO){
         File file = fileDTO.getFile();
