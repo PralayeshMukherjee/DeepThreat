674a2dec6732d882653f124e1e2222da3d8a0ae5
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-08-19 18:08:34 +0530

    add the post mapping annotation to hit the post request

--- Backend/src/main/java/com/DeepThreat/Controller/FileCheckerController.java
+++ Backend/src/main/java/com/DeepThreat/Controller/FileCheckerController.java
@@ -4,6 +4,7 @@ import com.DeepThreat.DTO.FileDTO;
 import com.DeepThreat.Service.FileScannerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,7 @@ import java.util.Map;
 public class FileCheckerController {
     @Autowired
     private FileScannerService fileScannerService;
+    @PostMapping("/check")
     public ResponseEntity<Map<String,String>> FileScan(FileDTO fileDTO){
         File file = fileDTO.getFile();
         String fileSafetyCheck = fileScannerService.checkFile(file);
