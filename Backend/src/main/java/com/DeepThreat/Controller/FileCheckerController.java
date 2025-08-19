a8da791b5539e69978299426df61ff401a17fc5f
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-08-19 17:51:07 +0530

    create a new method that return the ResponseEntity of Map of string of string

--- Backend/src/main/java/com/DeepThreat/Controller/FileCheckerController.java
+++ Backend/src/main/java/com/DeepThreat/Controller/FileCheckerController.java
@@ -1,9 +1,15 @@
 package com.DeepThreat.Controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/fileScanning")
 public class FileCheckerController {
+    public ResponseEntity<Map<String,String>> FileScan(){
+
+    }
 }
