2dffdeb9ed97952fdce79a121765d05ebba96731
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-08-19 17:40:42 +0530

    create a service method that take file as parameter and return data as string

--- Backend/src/main/java/com/DeepThreat/Service/FileScannerService.java
+++ Backend/src/main/java/com/DeepThreat/Service/FileScannerService.java
@@ -3,8 +3,13 @@ package com.DeepThreat.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+
 @Service
 public class FileScannerService {
     @Autowired
     private VirusTotalService virusTotalService;
+    public String checkFile(File file){
+
+    }
 }
