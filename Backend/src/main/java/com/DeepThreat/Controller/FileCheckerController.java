06667a946ed9173519cbfecc580748aded155613
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-08-19 17:48:51 +0530

    add a request mapping for this controller layer

--- Backend/src/main/java/com/DeepThreat/Controller/FileCheckerController.java
+++ Backend/src/main/java/com/DeepThreat/Controller/FileCheckerController.java
@@ -1,7 +1,9 @@
 package com.DeepThreat.Controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/fileScanning")
 public class FileCheckerController {
 }
