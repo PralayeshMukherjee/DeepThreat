a51d1e45dbfb46c24dd6c34930477d6102f259b2
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-08-19 18:09:56 +0530

    add a new request matcher and permit it

--- Backend/src/main/java/com/DeepThreat/Config/SecurityConfig.java
+++ Backend/src/main/java/com/DeepThreat/Config/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig {
                 .cors(Customizer.withDefaults())
                 .csrf(csrf->csrf.disable())
                 .authorizeHttpRequests(auth->auth
-                        .requestMatchers("/","/login/**","/oauth2/**","/user/**","/urlChecker/**","/userDetails/**","/lastData/**").permitAll()
+                        .requestMatchers("/","/login/**","/oauth2/**","/user/**","/urlChecker/**","/userDetails/**","/lastData/**","/fileScanning/**").permitAll()
                         .requestMatchers("/api/token-validation").permitAll()
                         .anyRequest().authenticated()
                 )
