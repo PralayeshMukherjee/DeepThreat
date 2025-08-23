99b6ea6c8e9e5d709e98c13a14c09d563638567c
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-08-23 21:43:23 +0530

    create the private data members of User Details DTO layer

--- Backend/src/main/java/com/DeepThreat/DTO/UserAllDetailsDTO.java
+++ Backend/src/main/java/com/DeepThreat/DTO/UserAllDetailsDTO.java
@@ -1,4 +1,12 @@
 package com.DeepThreat.DTO;
 
 public class UserAllDetailsDTO {
+    private String name;
+    private String email;
+    private String phone;
+    private int urlSearched;
+    private int totalDocumentScanned;
+    private int maliciousUrlCount;
+    private int suspiciousUrlCount;
+    private int safeUrlCount;
 }
