579990f2ce8e09c849d250f8b354ef62a91ab621
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-08-24 15:48:44 +0530

    set the phone of details dto layer that we get from the user entity

--- Backend/src/main/java/com/DeepThreat/Service/UserDetailsService.java
+++ Backend/src/main/java/com/DeepThreat/Service/UserDetailsService.java
@@ -45,5 +45,6 @@ public class UserDetailsService {
         UserEntity userEntity = getUserAllDetails(email);
         UserAllDetailsDTO detailsDTO = new UserAllDetailsDTO();
         detailsDTO.setName(userEntity.getName());
+        detailsDTO.setPhone(userEntity.getPhone());
     }
 }
