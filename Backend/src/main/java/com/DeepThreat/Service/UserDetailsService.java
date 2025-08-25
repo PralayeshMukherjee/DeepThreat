7590e4f074450a0d83e7aa33c75a9a4d4846e614
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-08-25 19:31:36 +0530

    set the safe url searched of the details dto object

--- Backend/src/main/java/com/DeepThreat/Service/UserDetailsService.java
+++ Backend/src/main/java/com/DeepThreat/Service/UserDetailsService.java
@@ -66,5 +66,6 @@ public class UserDetailsService {
         detailsDTO.setUrlSearched(countUrlSearched);
         detailsDTO.setMaliciousUrlCount(countMaliciousUrl);
         detailsDTO.setSuspiciousUrlCount(countSuspiciousUrl);
+        detailsDTO.setSafeUrlCount(countSafeUrl);
     }
 }
