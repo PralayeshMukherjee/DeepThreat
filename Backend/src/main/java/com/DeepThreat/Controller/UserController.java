6094eb6f0f8de190f940f48171618231808afacc
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-08-25 20:49:07 +0530

    create a new hashmap data structure

--- Backend/src/main/java/com/DeepThreat/Controller/UserController.java
+++ Backend/src/main/java/com/DeepThreat/Controller/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -66,5 +67,6 @@ public class UserController {
     }
     public ResponseEntity<Map<String,String>> getUserAllDetails(@RequestParam String email){
         UserAllDetailsDTO userAllDetailsDTO =userDetailsService.getUserIndepthDetails(email);
+        Map<String,String> map = new HashMap<>();
     }
 }
