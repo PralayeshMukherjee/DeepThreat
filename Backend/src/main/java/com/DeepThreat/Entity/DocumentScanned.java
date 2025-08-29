09a4c9a592154f0a9620b098024df486e2633900
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-08-29 23:53:01 +0530

    add the column name by using column annotation

--- Backend/src/main/java/com/DeepThreat/Entity/DocumentScanned.java
+++ Backend/src/main/java/com/DeepThreat/Entity/DocumentScanned.java
@@ -1,14 +1,13 @@
 package com.DeepThreat.Entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import org.antlr.v4.runtime.misc.NotNull;
 
 @Entity
 public class DocumentScanned {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int serialNo;
+    @Column(name = "email")
     private String email;
 }
