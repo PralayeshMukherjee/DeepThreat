b13b5f04e42889b866e67516f9aa6fa445d5c918
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-08-29 23:33:59 +0530

    add the id annotation to make the serial number as primary key

--- Backend/src/main/java/com/DeepThreat/Entity/DocumentScanned.java
+++ Backend/src/main/java/com/DeepThreat/Entity/DocumentScanned.java
@@ -1,8 +1,10 @@
 package com.DeepThreat.Entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Entity
 public class DocumentScanned {
+    @Id
     private int serialNo;
 }
