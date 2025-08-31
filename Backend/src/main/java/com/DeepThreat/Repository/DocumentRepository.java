f150a835c3ab4d1f108a3bf6ae90e7fc16a20261
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-08-31 19:42:37 +0530

    create a custom function that return integer which is the total document scanned by user

--- Backend/src/main/java/com/DeepThreat/Repository/DocumentRepository.java
+++ Backend/src/main/java/com/DeepThreat/Repository/DocumentRepository.java
@@ -4,4 +4,5 @@ import com.DeepThreat.Entity.DocumentScanned;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DocumentRepository extends JpaRepository<DocumentScanned,Long> {
+    int countTotalDocumentScanned();
 }
