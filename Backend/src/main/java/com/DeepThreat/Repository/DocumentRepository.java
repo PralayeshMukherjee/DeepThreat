c8fe59c117d2e318887947e668668159820db931
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-08-31 19:44:57 +0530

    add a query annotation to make custom query

--- Backend/src/main/java/com/DeepThreat/Repository/DocumentRepository.java
+++ Backend/src/main/java/com/DeepThreat/Repository/DocumentRepository.java
@@ -2,7 +2,9 @@ package com.DeepThreat.Repository;
 
 import com.DeepThreat.Entity.DocumentScanned;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface DocumentRepository extends JpaRepository<DocumentScanned,Long> {
+    @Query()
     int countTotalDocumentScanned();
 }
