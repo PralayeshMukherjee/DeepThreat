30d21b2637c2c93b386a3c382233d7e2481e4ded
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-08-31 19:52:14 +0530

    add the parameter of the method

--- Backend/src/main/java/com/DeepThreat/Repository/DocumentRepository.java
+++ Backend/src/main/java/com/DeepThreat/Repository/DocumentRepository.java
@@ -3,8 +3,9 @@ package com.DeepThreat.Repository;
 import com.DeepThreat.Entity.DocumentScanned;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface DocumentRepository extends JpaRepository<DocumentScanned,Long> {
     @Query(value = "select count(serial_no) from document_scanned where email= :email",nativeQuery = true)
-    int countTotalDocumentScanned();
+    int countTotalDocumentScanned(@Param("email")String email);
 }
